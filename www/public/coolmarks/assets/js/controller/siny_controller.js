83a681e32541f2d256e4c02ba65e2a1e06d0cda9
xiachen <chengpohi@gmail.com>
2015-05-13 19:11:57 +0800

    DONE:add localStorage save. --- TODO:remove save.

--- www/public/coolmarks/assets/js/controller/siny_controller.js
+++ www/public/coolmarks/assets/js/controller/siny_controller.js
@@ -1,12 +1,26 @@
-var siny = angular.module('siny', []);
-siny.controller('sinyCtrl', function($scope) {
+var siny = angular.module('siny', ['LocalStorageModule']);
+
+siny.config(function (localStorageServiceProvider) {
+    localStorageServiceProvider
+	.setPrefix('coolmarks')
+	.setStorageType('sessionStorage')
+	.setNotify(true, true)
+});
+
+siny.controller('sinyCtrl', function($scope, localStorageService) {
     var bookMarkName = $scope.bookMarkName;
     var bookMarkUrl = $scope.bookMarkUrl;
-    $scope.bookMarks = [];
+    
+    $scope.user = getItem("chengpohi") || { bookMarks: [] };
 
-    $scope.append = function (bookMarks, bookMarkName, bookMarkUrl) {
-	bookMarks.push({'url': bookMarkUrl, 'name': bookMarkName});
+    $scope.append = function (user, bookMarkName, bookMarkUrl) {
+	user.bookMarks.push({'url': bookMarkUrl, 'name': bookMarkName});
 	$scope.bookMarkName = '';
 	$scope.bookMarkUrl = '';
+	localStorageService.set("chengpohi", user);
     };
+    
+    function getItem(key) {
+	return localStorageService.get(key);
+    }
 });
